c10a1af7c74f853b678e3ddd75117e03b24f64c6
Noah Berman <noah@noahberman.org>
2016-10-04 20:29:08 -0600

    initial build

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,9 +2,47 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
-
+  set :method_override, true
+  set :public_folder, 'public'
+  
+  get '/' do
+    erb :dashboard
+  end
+  
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
+  
+  get '/robots/new' do
+    erb :new
+  end
+  
+  get '/robots/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+  
+  post '/robots' do
+    robot = Robot.new(params[:name])
+    robot.save
+    redirect '/robots'
+  end
+  
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+    
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:name])
+    redirect "robots/#{id}"
+  end
+  
+  delete '/robots/:id' do |id|
+    Robot.destroy(id.to_i)
+    redirect '/robots'
+  end
+  
 end
 
